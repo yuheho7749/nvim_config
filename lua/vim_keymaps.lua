af27f9d39046e0740cad16b5f3db58b82f465aa9
Joseph Ng <joseph_ng@ucsb.edu>
2024-02-05 01:38:42 -0800

    Initial nvim config

---
+++ lua/vim_keymaps.lua
@@ -0,0 +1,5 @@
+-- vim keymaps --
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ve", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>se", vim.cmd.Sex)
---
+++ lua/vim_options.lua
@@ -0,0 +1,10 @@
+-- vim options --
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.hlsearch = false
+
+
