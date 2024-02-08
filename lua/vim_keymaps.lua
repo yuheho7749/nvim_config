69710d415d29d907c279cc03fa08705263807866
Joseph Ng <joseph_ng@ucsb.edu>
2024-02-07 23:07:41 -0800

    Added window rotation/swap keymap

--- lua/vim_keymaps.lua
+++ lua/vim_keymaps.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<leader>wl", "<C-w>l")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wr", "<C-w>r")
 
 -- Visual Mode --
 -- vim.keymap.set("n", "v", "V")
