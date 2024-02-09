4f3e94e1b6fef30c6af4f3334187eccaf74c841b
Joseph Ng <joseph_ng@ucsb.edu>
2024-02-09 03:25:05 -0800

    Added dressing ui for commands

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,14 +1,5 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
-	{
 		"folke/todo-comments.nvim",
 		lazy = false,
 		dependencies = { "nvim-lua/plenary.nvim" },
