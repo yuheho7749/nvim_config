3917780749ff476c965da324e822a23ef0727df2
Joseph Ng <joseph_ng@ucsb.edu>
2024-02-11 00:36:28 -0800

    Updated telescope configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,25 @@
 return {
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope-file-browser.nvim',
+			'BurntSushi/ripgrep',
+		},
 		config = function()
+			local telescope = require('telescope')
+			telescope.setup({
+				extensions = {
+					file_browser = {
+						theme = 'ivy',
+						hijack_netw = true,
+
+					},
+				},
+
+			})
+			telescope.load_extension('file_browser')
+
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
