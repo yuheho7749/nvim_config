d5cfaa4fa9e81774caa8ce301ab2926063edd16d
Joseph Ng <yuheho7749@gmail.com>
2024-02-12 06:27:09 -0800

    Treesitter does not work with windows

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
-
-			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "cpp" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end
+		-- "nvim-treesitter/nvim-treesitter",
+		-- build = ":TSUpdate",
+		-- config = function ()
+		-- 	local configs = require("nvim-treesitter.configs")
+		--
+		-- 	configs.setup({
+		-- 		ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "cpp" },
+		-- 		sync_install = false,
+		-- 		highlight = { enable = true },
+		-- 		indent = { enable = true },
+		-- 	})
+		-- end
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
