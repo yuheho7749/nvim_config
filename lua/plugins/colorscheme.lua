0e58919ee03bf543d106e0440424d97424fad3db
Joseph Ng <joseph_ng@ucsb.edu>
2024-02-23 20:17:22 -0800

    Changed color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,18 @@ return {
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			-- load the colorscheme here
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+			vim.cmd([[colorscheme catppuccin-frappe]])
 		end,
 	},
 	{
