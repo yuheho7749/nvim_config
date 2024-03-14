c31af0d6d107a457c4456bf4f10f3cadf69f48e4
Joseph Ng <joseph_ng@ucsb.edu>
2024-03-14 02:16:46 -0700

    Cleaned up key remaps

--- lua/vim_keymaps.lua
+++ lua/vim_keymaps.lua
@@ -1,14 +1,10 @@
--- keymaps --
+-- Keymaps --
 vim.g.mapleader = " "
-
 
 -- Window Manipulation --
 -- vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 -- vim.keymap.set("n", "<leader>ve", vim.cmd.Vex)
 -- vim.keymap.set("n", "<leader>se", vim.cmd.Sex)
-vim.keymap.set("n", "<leader>vs", vim.cmd.vs)
-vim.keymap.set("n", "<leader>sp", vim.cmd.sp)
-
 vim.keymap.set("n", "<leader>vs", vim.cmd.vs)
 vim.keymap.set("n", "<leader>sp", vim.cmd.sp)
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
@@ -17,29 +13,13 @@ vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wr", "<C-w>r")
 
-
 -- Visual Block Mode --
 vim.keymap.set("n", "<A-v>", "<C-v>")
 
 -- Visual Mode --
--- vim.keymap.set("n", "v", "V")
--- vim.keymap.set("n", "V", "v")
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- vim.keymap.set("v", "H", "b")
--- vim.keymap.set("v", "L", "w")
--- vim.keymap.set("v", "<a-h>", "0")
--- vim.keymap.set("v", "<a-l>", "$")
-
 
 -- Cursor Movement --
--- vim.keymap.set({"n", "v"}, "H", "b")
--- vim.keymap.set({"n", "v"}, "L", "w")
 vim.keymap.set({"n", "v"}, "H", "_")
 vim.keymap.set({"n", "v"}, "L", "$")
-
-
--- Delete word --
--- vim.keymap.set("n", "D", "diw")
-
